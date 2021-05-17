70be5116c8a11918540d2abfa1cd63df9b7eacb9
Renê <renejr.arraes286@gmail.com>
2021-05-17 09:12:49 -0300

    adding some questions

--- simulados/js/matematica-questoes.js
+++ simulados/js/matematica-questoes.js
@@ -1,27 +1,29 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [{
         numb: 1,
-        question: "What does HTML stand for?",
-        answer: "Hyper Text Markup Language",
+        question: "Para atender a alta demanda em smartphones, uma fábrica decidiu aumentar o número de produtos produzidos diariamente. Para isso, ela investiu em mais 3 máquinas, totalizando-se 8 máquinas. Sabendo-se que eram produzidos diariamente 750 smartphones, haverá um aumento na produção diária de:",
+        answer: "450",
         options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Language",
-            "Hyper Text Multiple Language",
-            "Hyper Tool Multi Language"
+
+            "1200",
+            "1000",
+            "350",
+            "450"
         ]
     },
     {
         numb: 2,
-        question: "What does CSS stand for?",
-        answer: "Cascading Style Sheet",
+        question: "Para encher um tanque de água do condomínio, 5 torneiras levam exatamente 9 horas. Supondo-se que a vazão das torneiras seja sempre a mesma, quanto tempo levaria o enchimento do tanque se fossem apenas 3 torneiras?",
+        answer: "15 horas",
+
+
         options: [
-            "Common Style Sheet",
-            "Colorful Style Sheet",
-            "Computer Style Sheet",
-            "Cascading Style Sheet"
+            "15 horas",
+            "12 horas",
+            "10 horas",
+            "8 horas",
         ]
-    },
-    {
+    }, {
         numb: 3,
         question: "What does PHP stand for?",
         answer: "Hypertext Preprocessor",
@@ -31,8 +33,7 @@ let questions = [{
             "Hypertext Preprogramming",
             "Hometext Preprocessor"
         ]
-    },
-    {
+    }, {
         numb: 4,
         question: "What does SQL stand for?",
         answer: "Structured Query Language",
@@ -42,8 +43,7 @@ let questions = [{
             "Statement Question Language",
             "Structured Query Language"
         ]
-    },
-    {
+    }, {
         numb: 5,
         question: "What does XML stand for?",
         answer: "eXtensible Markup Language",
