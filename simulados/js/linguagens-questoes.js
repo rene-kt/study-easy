bf0523f1466d976c3217cc44353e30e744733ca0
Renê <renejr.arraes286@gmail.com>
2021-05-18 08:24:10 -0300

    create linguagens simulado

---
+++ simulados/js/linguagens-questoes.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "A forma fixa caracterizada por versos heroicos ou alexandrinos dispostos em duas quadras e dois tercetos é chamada:",
+        answer: "soneto.",
+
+        options: [
+
+            "haicai.",
+            "oitava.",
+            "soneto.",
+            "décima."
+        ]
+    },
+    {
+        numb: 2,
+        question: " O gênero dramático, entre outros aspectos, apresenta como característica essencial:",
+        answer: "a estrutura dialógica.",
+
+
+
+        options: [
+            "a presença de um narrador.",
+            "a estrutura dialógica.",
+            "o extravasamento lírico.",
+            "a musicalidade.",
+        ]
+    }, {
+        numb: 3,
+        question: " O soneto é uma das formas poéticas mais tradicionais e difundidas nas literaturas ocidentais e expressa, quase sempre, conteúdo:",
+        answer: "lírico.",
+
+
+
+        options: [
+            "dramático.",
+            "satírico.",
+            "lírico.",
+            "épico.",
+        ]
+    }, {
+        numb: 4,
+        question: "Com relação aos fundamentos da educação teatral e das pedagogias do teatro, julgue os itens que se seguem. Quando o teatro é utilizado como ferramenta para a aprendizagem de outras disciplinas (tais como história, geografia, português), potencializa‐se a aprendizagem da linguagem teatral e a ação pedagógica se configura como interdisciplinar.",
+        answer: "Certo",
+        options: [
+            "Certo",
+            "Errado",
+            "",
+            "",
+        ]
+    }, {
+        numb: 5,
+        question: "No que se refere ao ensino de arte no Brasil, julgue os próximos itens. A cultura visual na educação propõe que a visualidade do cotidiano (outdoors etc.) seja lida criticamente pelos alunos, a fim de que aprendam a distinguir e valorizar a arte erudita.",
+        answer: "Errado",
+        options: [
+            "Certo",
+            "Errado",
+            "",
+            "",
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Identifique a frase em que a função da linguagem predominante é a função referencial.",
+        answer: "Existem três acentos gráficos na língua portuguesa.",
+        options: [
+            "Existem três acentos gráficos na língua portuguesa.",
+            "Estou muito animada com o meu novo emprego.",
+            "Siga o meu exemplo. Você se sentirá melhor!",
+            "Sim... Sei… Estou ouvindo, claro.",
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "Qual a função da linguagem presente na frase: “Ligue agora! Não perca esta oportunidade!”",
+        answer: "Função apelativa",
+        options: [
+            "Função expressiva",
+            "Função apelativa",
+            "Função metalinguística",
+            "Função fática",
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "Em qual função da linguagem a ênfase é dada ao contexto comunicativo, tendo como principal objetivo informar o receptor da mensagem sobre um assunto específico?",
+        answer: "Função referencial ou denotativa",
+        options: [
+            "Função conotativa",
+            "Função metalinguística",
+            "Função fática",
+            "Função referencial ou denotativa",
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "Dê a função sintática dos termos assinalados pelas aspas: 'O lucro', que é um dos incentivos do sistema, foi 'excelente'.",
+
+
+        answer: "sujeito - predicativo do sujeito.",
+        options: [
+            "objeto direto - adjunto adverbial.",
+            "sujeito - predicativo do sujeito.",
+            "sujeito - predicativo do objeto.",
+            "predicativo do sujeito - predicativo do objeto.",
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "'Em nossa terra não se vive senão de política.' Nesta oração o sujeito é:",
+
+
+        answer: "indeterminado",
+        options: [
+            "indeterminado",
+            "oração sem sujeito",
+            "oculto",
+            "simples",
+        ]
+    },
+
+];
