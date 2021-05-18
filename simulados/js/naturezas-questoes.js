8983ab45707d43d3c7b7f9ff7ebc9393af98ffe4
Renê <renejr.arraes286@gmail.com>
2021-05-18 19:42:02 -0300

    simulado naturezas

---
+++ simulados/js/naturezas-questoes.js
@@ -0,0 +1,130 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Um veículo trafega em uma rodovia com velocidade média de 80 km/h. Sabendo que a viagem teve uma duração de 1 hora e 30 minutos (1,5 h), qual foi a distância percorrida pelo veículo?",
+        answer: "120 km",
+        options: [
+
+            "80 km",
+            "120 km",
+            "160 km",
+            "100 km",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Um veículo automotivo acelera a 2,0 m/s², durante 5,0 s, a partir de uma velocidade inicial de 2,0 m/s. A distância percorrida por esse veículo, durante esse intervalo de tempo, é igual a:",
+        answer: "25 m",
+
+        options: [
+            "30 m",
+            "25 m",
+            "20 m",
+            "15 m",
+        ]
+    }, {
+        numb: 3,
+        question: "Um motorista dirigia a 30 m/s quando avista um buraco na pista e pisa no freio. Os freios produziram uma desaceleração de 2,0 m/s², até que o carro para completamente. O espaço percorrido pelo veículo até o final da frenagem é de:",
+        answer: "350 m",
+
+        options: [
+            "150 m",
+            "250 m",
+            "350 m",
+            "450 m",
+        ]
+    }, {
+        numb: 4,
+        question: "Uma locomotiva afasta-se de um observador enquanto sua velocidade aumenta a cada segundo. O movimento descrito por essa locomotiva pode ser classificado como:",
+        answer: "Progressivo e acelerado",
+        options: [
+            "Progressivo e acelerado",
+            "Progressivo e retardado",
+            "Progressivo e acelarado",
+            "Progressivo e retardado",
+        ]
+    }, {
+        numb: 5,
+        question: "Uma planta medicinal utilizada para regular a glicemia é encontrada na região amazônica, sendo popularmente conhecida como pata-de-vaca."
+
+            +
+            "A espécie que funciona como uma “insulina vegetal” possui entre seus compostos químicos um alcano, cuja fórmula contém 74 átomos de hidrogênio. Portanto, o número de átomos de carbono presentes na cadeia carbônica é:",
+        answer: "36",
+        options: [
+            "34",
+            "35",
+            "36",
+            "37",
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "(PUC-PR) Alcinos são hidrocarbonetos:",
+        answer: "alifáticos saturados.",
+
+        options: [
+            "alifáticos saturados.",
+            "alicíclicos saturados.",
+            "alicíclicos insaturados com tripla ligação.",
+            "alifáticos insaturados com tripla ligação.",
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "(Unesp) O octano é um dos principais constituintes da gasolina, que é uma mistura de hidrocarbonetos. A fórmula molecular do octano é:",
+        answer: "C8H24",
+
+        options: [
+            "C8H18",
+            "C8H24",
+            "C8H32",
+            "C8H38",
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "Os líquens são formados pela associação de certas espécies de algas e um fungo. Ambas as espécies são beneficiadas nessa relação, sendo que uma espécie não é capaz de viver isoladamente naquele local. Nesse caso, há uma relação chamada de:",
+        answer: "Comensalismo.",
+
+        options: [
+            "Comensalismo.",
+            "Inquilinismo.",
+            "Mutualismo.",
+            "Protocooperação",
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "A dispersão de sementes é essencial para a sobrevivência de uma planta, uma vez que a grande quantidade de sementes próximas à planta-mãe diminui a chance de sobrevivência. Ao separar as sementes por uma área maior, cada uma terá acesso a uma quantidade maior de recursos, evitando assim:",
+
+
+        answer: "Competição",
+        options: [
+            "Competição",
+            "Parasitismo",
+            "Comensalismo",
+            "Predação",
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "Algumas relações ecológicas causam benefício para apenas uma espécie, não prejudicando nem beneficiando a outra. Um exemplo clássico ocorre entre a rêmora e o tubarão. Nesse caso, a rêmora prende-se ao corpo do tubarão e alimenta-se dos restos de suas presas."
+
+            +
+            "Marque a alternativa que indica corretamente o nome dessa relação ecológica.",
+
+        answer: "Amensalismo",
+        options: [
+            "Inquilinismo",
+            "Mutualismo",
+            "Amensalismo",
+            "Comensalismo",
+        ]
+    },
+
+];
